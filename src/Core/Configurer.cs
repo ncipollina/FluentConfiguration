54934aefdafacce111c596bed1e3bddc06b52971
Nick Cipollina <ncipollina@gmail.com>
2017-01-24 15:10:58 -0500

    Added more logic.

--- src/Core/Configurer.cs
+++ src/Core/Configurer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapTech.FluentConfiguration.Core.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +9,42 @@ namespace CapTech.FluentConfiguration.Core
 {
     public class Configurer : IConfigurer
     {
-        private static IConfigurer _instance;
+        private static volatile IConfigurer _instance;
+        private static object syncRoot = new object();
+
+        private readonly IList<PipelineConfiguration> _pipelineConfigs = new List<PipelineConfiguration>();
+
+        private Configurer() { }
         public static IConfigurer Instance
         {
             get
             {
                 if (_instance == null)
-                    throw new InvalidOperationException("Configurer not initialized.");
-
+                {
+                    lock (syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Configurer();
+                        }
+                    }
+                }
                 return _instance;
             }
 
             private set { _instance = value; }
         }
 
-        public static void Initialize(Action<IConfigurationExpression> element)
+
+        public void AddPipeline(PipelineConfiguration pipelineConfig)
         {
-            Instance = new Configurer();
+            _pipelineConfigs.Add(pipelineConfig);
+        }
+
+
+        public void AddPipeline<TPipeline>(TPipeline pipelineConfig) where TPipeline : PipelineConfiguration, new()
+        {
+            AddPipeline(pipelineConfig);
         }
     }
 }
--- src/Core/IConfigurer.cs
+++ src/Core/IConfigurer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapTech.FluentConfiguration.Core.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace CapTech.FluentConfiguration.Core
 {
     public interface IConfigurer
     {
+        void AddPipeline(PipelineConfiguration pipelineConfig);
+        void AddPipeline<TPipeline>(TPipeline pipelineConfig) where TPipeline : PipelineConfiguration, new();
     }
 }
